705f2fed5927997b40c4f8edbc3a60be9eda1736
fekho2558 <49279583+fekho2558@users.noreply.github.com>
2019-05-30 09:17:25 -0400

    Add clock to every page (ADD CLOCK)

---
+++ javascript.js
@@ -0,0 +1,22 @@
+
+function updateTime() {
+	var currentTime = new Date ( );
+
+	var currentHours = currentTime.getHours ( );
+	var currentMinutes = currentTime.getMinutes ( );
+	var currentSeconds = currentTime.getSeconds ( );
+
+	currentMinutes = (currentMinutes < 10 ? "0": "") + currentMinutes;
+	currentSeconds = (currentSeconds < 10 ? "0": "") + currentSeconds;
+
+	var timeOfDay = (currentHours < 12 ) ? "AM" : "PM";
+	currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+	currentHours = (currentHours == 0) ? 12 : currentHours;
+
+	var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+	document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+
+}
+
+window.onload = updateTime;
+window.setInterval(updateTime, 1000);
