efd018e1ceab60d52c110c18b30e0936a20cce03
CalebCallaghan <37084042+CalebCallaghan@users.noreply.github.com>
2019-06-11 14:26:29 -0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
+function loadpage() {
+	updateTime();
+	carousel();
+}
+
 //clock
 function updateTime() {
 	var currentTime = new Date ( );
@@ -18,6 +23,22 @@ function updateTime() {
 
 }
 
-window.onload = updateTime;
 window.setInterval(updateTime, 1000);
 //clock
+
+// Automatic Slideshow - change image every 3 seconds
+var myIndex = 0;
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  x[myIndex-1].style.display = "block";
+  setTimeout(carousel, 3000);
+}
+
+window.onload = loadpage;
